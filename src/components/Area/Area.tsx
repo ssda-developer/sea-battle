5c56a6d584fef3770c96a6011391c70994c81b43
Sanya MARYNYCH <sanyamarynych@o-s-i.org>
2021-02-04 01:18:46 +0200

    - rebuild squares

--- src/components/Area/Area.tsx
+++ src/components/Area/Area.tsx
@@ -1,47 +1,14 @@
-import React, { FC, MouseEvent, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { FC } from 'react';
 
-import { RootStore } from '../../redux/store';
-import { renderFriendlySquare, renderEnemySquare, changeOwns } from '../../redux/Area/areaActions';
-import { IOwns, Owns } from '../../redux/Area/areaInterfaces';
+import { IOwns } from '../../redux/Area/areaInterfaces';
 
 import { AREA_LETTERS, AREA_NUMBERS } from '../../constants/areaConstants';
-
-import { addShip, updateCell } from '../../redux/Area/areaUtils';
 
 import BuildSquare from '../Square/BuildSquare/BuildSquare';
 
 import './Area.scss';
 
 const Area: FC<IOwns> = (owns: IOwns) => {
-    const dispatch = useDispatch();
-    const areaState = useSelector((state: RootStore) => state.areaReducer);
-    const shipsState = useSelector((state: RootStore) => state.shipsReducer);
-
-    let { friendlySquare, enemySquare } = areaState.squares;
-
-    useEffect(() => {
-        dispatch(renderFriendlySquare(friendlySquare));
-        dispatch(renderEnemySquare(enemySquare));
-        dispatch(changeOwns(owns));
-    }, []);
-
-    const updateCellHandler = (evn: MouseEvent<HTMLButtonElement>) => {
-        evn.preventDefault();
-
-        const {
-            currentTarget: { id },
-        } = evn;
-        const { currentShip } = shipsState;
-        if (owns.owns === 'FRIENDLY') {
-            dispatch(renderFriendlySquare(addShip(friendlySquare, id)));
-        } else {
-            dispatch(renderEnemySquare(updateCell(friendlySquare, id)));
-        }
-    };
-
-    const currentSquare = owns.owns === Owns.Friendly ? friendlySquare : enemySquare;
-
     return (
         <div className="area">
             <div className="area__letters">
@@ -59,7 +26,7 @@ const Area: FC<IOwns> = (owns: IOwns) => {
                 ))}
             </div>
             <div className="area__wrapper">
-                <BuildSquare currentSquare={currentSquare} cellHandler={updateCellHandler} />
+                <BuildSquare playerAffiliation={owns} />
             </div>
         </div>
     );
--- src/components/Square/BuildSquare/BuildSquare.tsx
+++ src/components/Square/BuildSquare/BuildSquare.tsx
@@ -1,42 +1,78 @@
-import React, { FC, MouseEvent } from 'react';
+import React, { FC, MouseEvent, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { IField } from '../../../redux/Field/fieldInterfaces';
+import { IOwns, Owns } from '../../../redux/Area/areaInterfaces';
 
 import { AREA_LETTERS, AREA_NUMBERS } from '../../../constants/areaConstants';
+import { addShip, updateCell } from '../../../redux/Area/areaUtils';
+
+import { RootStore } from '../../../redux/store';
+import { renderEnemySquare, renderFriendlySquare } from '../../../redux/Area/areaActions';
+
 import FieldRow from '../../FieldRow/FieldRow';
 
 interface BuildSquareProps {
-    currentSquare: Array<Array<IField>>;
-    cellHandler: (evn: MouseEvent<HTMLButtonElement>) => void;
+    playerAffiliation: IOwns;
 }
 
-const BuildSquare: FC<BuildSquareProps> = ({ currentSquare, cellHandler }: BuildSquareProps) => {
+const BuildSquare: FC<BuildSquareProps> = ({ playerAffiliation }: BuildSquareProps) => {
+    const dispatch = useDispatch();
+    const areaState = useSelector((state: RootStore) => state.areaReducer);
+
+    const { friendlySquare, enemySquare } = areaState.squares;
     const square: Array<Array<IField>> = [];
 
-    AREA_NUMBERS.forEach(number => {
-        const row: Array<IField> = [];
+    // const currentSquare = playerAffiliation.owns === Owns.Friendly ? friendlySquare : enemySquare;
 
-        AREA_LETTERS.forEach(letter => {
-            const cell = {
-                id: `${number}${letter}`,
-                ship: false,
-                shipId: '',
-                hit: false,
-                past: false,
-                locked: false,
-                lockedId: '',
-            };
+    const createSquare = () => {
+        AREA_NUMBERS.forEach(number => {
+            const row: Array<IField> = [];
 
-            row.push(cell);
+            AREA_LETTERS.forEach(letter => {
+                const cell = {
+                    id: `${number}${letter}`,
+                    ship: false,
+                    shipId: '',
+                    hit: false,
+                    past: false,
+                    locked: false,
+                    lockedId: '',
+                };
+
+                row.push(cell);
+            });
+
+            square.push(row);
         });
 
-        square.push(row);
-    });
+        return square;
+    };
+
+    useEffect(() => {
+        const currentSquare = createSquare();
+        dispatch(renderFriendlySquare(currentSquare));
+        dispatch(renderEnemySquare(currentSquare));
+    }, []);
+
+    const updateCellHandler = (evn: MouseEvent<HTMLButtonElement>) => {
+        evn.preventDefault();
+
+        const {
+            currentTarget: { id },
+        } = evn;
+
+        if (playerAffiliation.owns === 'FRIENDLY') {
+            dispatch(renderFriendlySquare(addShip(friendlySquare, id)));
+        } else {
+            dispatch(renderEnemySquare(updateCell(friendlySquare, id)));
+        }
+    };
 
     return (
         <>
-            {square.map((row: IField[], idx: number) => (
-                <FieldRow key={row[idx].id} row={row} updateCellHandler={cellHandler} />
+            {friendlySquare.map((row: IField[], idx: number) => (
+                <FieldRow key={row[idx].id} row={row} updateCellHandler={updateCellHandler} />
             ))}
         </>
     );

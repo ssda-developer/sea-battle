95cf1535ba5b9006069ae41ecd95aa7d99109927
Sanya MARYNYCH <sanyamarynych@o-s-i.org>
2021-04-28 22:51:04 +0300

    - added winner message component

---
+++ src/components/WinnerMessage/WinnerMessage.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+
+import { Owners } from '../../store/area/interfaces';
+
+import { ReactComponent as SVGFrown } from '../../icons/frown.svg';
+import { ReactComponent as SVGSmile } from '../../icons/smile.svg';
+
+import './WinnerMessage.scss';
+
+type WinnerMessageProps = {
+    player: Owners | null;
+};
+
+const WinnerMessage: FC<WinnerMessageProps> = ({ player }: WinnerMessageProps) => {
+    const { User } = Owners;
+    const message = player === User ? 'Поздравляем! Вы победили!' : 'Вы проиграли';
+    return (
+        <div className="winner-message">
+            <h2 className="winner-message__title">
+                {message}
+                {player === User ? <SVGSmile /> : <SVGFrown />}
+            </h2>
+        </div>
+    );
+};
+
+export default WinnerMessage;
